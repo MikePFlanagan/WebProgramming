c183953d74d9c872ee5c5e017903c35b6e5e1c46
Mike Flanagan <41282720+MikePFlanagan@users.noreply.github.com>
2020-02-05 20:44:02 -0800

    week3
    JSON string issues

--- Week 3/js/students.js
+++ Week 3/js/students.js
@@ -1,25 +1,23 @@
-var students = [
-    {StudentID: 1, EmailAddress: "M.Jackson@ABCSchool.com"},
-     {StudentID: 2, EmailAddress: "J.Jackson@ABCSchool.com"},
-      {StudentID: 3, EmailAddress: "R.Jackson@ABCSchool.com"},
-      {StudentID: 4, EmailAddress: "T.Jackson@ABCSchool.com"},
-       {StudentID: 5, EmailAddress: "J.Jackson@ABCSchool.com"} ];
-var studentsJSON = JSON.stringify(students);
+var students = '{ "students" : [ {"StudentID" : 1, "EmailAddress": "M.Jackson@ABCSchool.com"},{"StudentID": 2, "EmailAddress" : "Jackson@ABCSchool.com"},      {"StudentID": 3, "EmailAddress": "R.Jackson@ABCSchool.com"}, {"StudentID": 4, "EmailAddress": "T.Jackson@ABCSchool.com"},{"StudentID": 5, "EmailAddress":"J.Jackson@ABCSchool.com"} ]}'
+       
+ function initializeStudents(){
+     var data = getStudents();
+     displayStudents(data.students);
+ }      
 
-function getStudents(){
+function getStudents(students){
 return JSON.parse(students);
 }
 
-var getdata = getStudents();
-displayStudents();
+
 
 function displayStudents(students){
     var tableList = "";
     for(i = 0; i < students.length; i++){
         var StudentID = students[i].StudentID;
         var EmailAddress = students[i].EmailAddress;
-        tableList +=  "<tr><td>" + studentID + "</t></td>" + price + "</td>";
-
-        DocumentFragment.getElementById("studentsJSON").innerHTML = tableList;
+        tableList += "<tr><td>" + studentID + "</t><td>" + price + "</td>";
+    
+        document.getElementById("Students-list").innerHTML = tableList;
     }
 }
