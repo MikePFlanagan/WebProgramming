a43085435b10aef18a4f6dfb6d5d9c49b9dbdafe
Michael <mpflan83@gmail.com>
2020-03-21 15:47:21 -0700

    cleanup

--- Week 8/webapi/Services/BusinessRules.cs
+++ Week 8/webapi/Services/BusinessRules.cs
@@ -8,8 +8,10 @@ public static class BusinessRules
     public static bool isInvalidEmail(Student student) =>
         !student.EmailAddress.ToLower().Contains(atSymbol);
 
-    public static bool isSpecial(Student student) =>
-       student.StudentId > MaxStudentsAllowed || !student.EmailAddress.ToLower().Contains(atSymbol);
+    // public static bool isSpecial(Student student) =>
+    //    student.StudentId > MaxStudentsAllowed || !student.EmailAddress.ToLower().Contains(atSymbol);
+
+    public static bool isSpecial(Student student)=> isInvalidEmail(student) || isOverQuota(student);
 }
 
     
